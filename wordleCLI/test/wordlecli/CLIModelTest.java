3a1d959aed2d17f98d2f85621f01b94baf9263e0
Cuthbert-cloud <56514564+Cuthbert-cloud@users.noreply.github.com>
2022-02-06 22:37:26 +0000

    Tests for Model #2

--- wordleCLI/test/wordlecli/CLIModelTest.java
+++ wordleCLI/test/wordlecli/CLIModelTest.java
@@ -1,36 +1,102 @@
 package wordlecli;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CLIModelTest {
-    
-    public CLIModelTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+
+    /**
+     * Test of getRowCount method, of class CLIModel.
+     */
+    @Test
+    public void testGetRowCount() {
+        System.out.println("getRowCount");
+        CLIModel instance = new CLIModel();
+        int expResult = 0;
+        int result = instance.getRowCount();
+        assertEquals(expResult, result);
     }
 
+    /**
+     * Test of getColCount method, of class CLIModel.
+     */
     @Test
-    public void testSomeMethod() {
-        
+    public void testGetColCount() {
+        System.out.println("getColCount");
+        CLIModel instance = new CLIModel();
+        int expResult = 0;
+        int result = instance.getColCount();
+        assertEquals(expResult, result);
     }
-    
+
+    /**
+     * Test of countGuesses method, of class CLIModel.
+     */
+    @Test
+    public void testCountGuesses() {
+        System.out.println("countGuesses");
+        CLIModel instance = new CLIModel();
+        int expResult = 0;
+        int result = instance.countGuesses();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getGuess method, of class CLIModel.
+     */
+    @Test
+    public void testGetGuess() {
+        System.out.println("getGuess");
+        int index = 0;
+        CLIModel instance = new CLIModel();
+        String expResult = "";
+        String result = instance.getGuess(index);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getAnswer method, of class CLIModel.
+     */
+    @Test
+    public void testGetAnswer() {
+        System.out.println("getAnswer");
+        CLIModel instance = new CLIModel();
+        String expResult = "";
+        String result = instance.getAnswer();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of hasWon method, of class CLIModel.
+     */
+    @Test
+    public void testHasWon() {
+        System.out.println("hasWon");
+        CLIModel instance = new CLIModel();
+        boolean expResult = false;
+        boolean result = instance.hasWon();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of isGameOver method, of class CLIModel.
+     */
+    @Test
+    public void testIsGameOver() {
+        System.out.println("isGameOver");
+        CLIModel instance = new CLIModel();
+        boolean expResult = false;
+        boolean result = instance.isGameOver();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of guess method, of class CLIModel.
+     */
+    @Test
+    public void testGuess() {
+        System.out.println("guess");
+        String str = "";
+        CLIModel instance = new CLIModel();
+        instance.guess(str);
 }
