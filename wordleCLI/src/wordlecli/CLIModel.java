f201acdca64bedf8d96d3bac018245b22d710673
Cuthbert-cloud <56514564+Cuthbert-cloud@users.noreply.github.com>
2022-02-07 22:25:26 +0000

    update Model #4

--- wordleCLI/src/wordlecli/CLIModel.java
+++ wordleCLI/src/wordlecli/CLIModel.java
@@ -1,59 +1,113 @@
 package wordlecli;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class CLIModel {
 
     private final int rowCount;
     private final int colCount;
-    private ArrayList<String> guesses;
+    private final ArrayList<String> guesses;
+    private List<String> words;
     private final String answer;
-    private String filePath;
+    private final String filePath;
 
-    public CLIModel() {
+    public CLIModel() throws IOException {
+        this.filePath = "dictonary/common.txt";
+        initialised(filePath);
         this.rowCount = 5;
         this.colCount = 6;
-        this.guesses = new ArrayList<String>();
-        this.filePath = "words.txt";
-        this.answer = generateAnswer(filePath);
+        this.guesses = new ArrayList<>();
+        this.answer = generateAnswer();
     }
 
+    /**
+     *
+     * @return
+     */
     public int getRowCount() {
         return rowCount;
     }
     
+    /**
+     *
+     * @return
+     */
     public int getColCount() {
         return colCount;
     }
     
+    /**
+     *
+     * @return
+     */
     public int countGuesses() {
         return guesses.size();
     }
     
+    /**
+     *
+     * @param index
+     * @return
+     */
     public final String getGuess(int index) {
         return guesses.get(index);
     }
     
+    /**
+     *
+     * @return
+     */
     public final String getAnswer() {
         return answer;
     }
     
+    /**
+     *
+     * @return
+     */
     public boolean hasWon() {
         return guesses.contains(getAnswer());
     }
     
+    /**
+     *
+     * @return
+     */
     public boolean isGameOver() {
         return hasWon() || countGuesses() == getColCount();
     }
     
+    /**
+     *
+     * @param str
+     */
     public void guess(String str) {
         if (str.length() == getRowCount()) {
             guesses.add(str);
         }
     }
     
-    private String generateAnswer(String filePath) {
-        return null;
+    /**
+     *
+     * @param filePath
+     */
+    private void initialised(String filePath) throws IOException {
+        this.words = Files.readAllLines(Paths.get(filePath))
+                .stream().collect(Collectors.toList());
+    }
+    
+    /**
+     *
+     * @return
+     */
+    private String generateAnswer() {
+        return words.get(new Random().nextInt(words.size()));
     }
 }
 
