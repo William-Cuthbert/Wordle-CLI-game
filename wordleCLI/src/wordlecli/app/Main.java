1c8734791f934aa510f3e4dd3df45b3ad34d31e6
Cuthbert-cloud <56514564+Cuthbert-cloud@users.noreply.github.com>
2022-02-25 19:03:58 +0000

    update main

--- wordleCLI/src/wordlecli/app/Main.java
+++ wordleCLI/src/wordlecli/app/Main.java
@@ -8,10 +8,13 @@ import wordlecli.views.View;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
         boolean debugger = true;
         Wordle wordle_game = new Wordle();
+
         try (Scanner guess = new Scanner(System.in)) {
             String input;
+
             while (!wordle_game.isGameOver()) {
                 View.cleanUp();
                 View.printGameBoard(wordle_game);
@@ -24,17 +27,26 @@ public class Main {
                 input = guess.nextLine();
 
                 while (!View.validateInput(input)) {
+                    System.out.println("Please use a-z A-Z characters or write 5 letters only");
                     View.requestInput();
                     input = guess.nextLine();
                 }
 
                 wordle_game.addGuess(input);
             }
+
             View.cleanUp();
             View.printGameBoard(wordle_game);
             View.printAnswer(wordle_game.getAnswer());
             guess.close();
+
+            if (wordle_game.hasLost()) {
+                System.out.println("==================\n     You lose!\n==================");
+            } 
+            
+            if (wordle_game.hasWon()) {
+                System.out.println("==================\n    Well done!\n==================");
+            }
         }
     }
-
 }
