7777fe58e2749cedc235c8c333bc0a0b4fec8d5a
Cuthbert-cloud <56514564+Cuthbert-cloud@users.noreply.github.com>
2022-02-26 20:18:51 +0000

    update main #3

--- wordleCLI/src/wordlecli/app/Main.java
+++ wordleCLI/src/wordlecli/app/Main.java
@@ -3,44 +3,43 @@ package wordlecli.app;
 import java.io.IOException;
 import java.util.Scanner;
 import wordlecli.models.Wordle;
-import wordlecli.views.View;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        
+        Wordle wordle = new Wordle();
+        wordle.setDebug(false);
 
-        boolean debugger = true;
-        Wordle wordle_game = new Wordle();
-
-        try (Scanner guess = new Scanner(System.in)) {
+        try (Scanner guessWord = new Scanner(System.in)) {
             String input;
 
-            while (!wordle_game.isGameOver()) {
-                View.cleanUp();
-                View.printGameBoard(wordle_game);
+            while (!wordle.isGameOver()) {
+                CommandLine.cleanUp();
+                CommandLine.printGameBoard(wordle);
 
-                if (debugger) {
-                    View.printAnswer(wordle_game.getAnswer());
+                if (wordle.getDebug()) {
+                    CommandLine.displayAnswer(wordle.getAnswer());
                 }
 
-                View.requestInput();
-                input = guess.nextLine();
+                CommandLine.requestInput();
+                input = guessWord.nextLine();
                 
-                while (wordle_game.notInDictionary(input)) {
-                    View.validating_message();
-                    View.requestInput();
-                    input = guess.nextLine();
+                while (wordle.notInDictionary(input)) {
+                    CommandLine.validateMessage();
+                    CommandLine.requestInput();
+                    input = guessWord.nextLine();
                 }
 
-                wordle_game.addGuess(input);
+                wordle.addGuess(input);
             }
 
-            View.cleanUp();
-            View.printGameBoard(wordle_game);
-            View.printAnswer(wordle_game.getAnswer());
-            guess.close();
+            CommandLine.cleanUp();
+            CommandLine.printGameBoard(wordle);
+            CommandLine.displayAnswer(wordle.getAnswer());
+            guessWord.close();
 
-            View.result_message(wordle_game);
+            CommandLine.resultMessage(wordle);
         }
     }
 }
